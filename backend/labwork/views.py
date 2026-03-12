22cc3d9fd041c74f649ccc54cf7300ca304914ad
Aayush Chhuka <aayushchhuka7@gmail.com>
2026-03-12 17:25:03 +0545

    change make in samfe file so no code for previous question

---
+++ backend/labwork/views.py
@@ -0,0 +1,24 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.parsers import MultiPartParser, FormParser
+from .serializers import RegistrationSerializer
+from .models import Registration
+
+class RegistrationView(APIView):
+    parser_classes = [MultiPartParser, FormParser]
+
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'message': 'Form submitted successfully!',
+                'data': serializer.data
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        registrations = Registration.objects.all()
+        serializer = RegistrationSerializer(registrations, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
